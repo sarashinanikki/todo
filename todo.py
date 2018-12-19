051c76cca4bba9d6037bc0b8f7cee24b979db119
sarashin <sarashina.nikki65@gmail.com>
2018-12-19 13:47:42 +0900

    erase completegit add todo.py

--- todo.py
+++ todo.py
@@ -23,57 +23,62 @@ def add(args):
     if len(args) <= 1:
         alart = 'TODOの件名は必須となります'
         return alart
-    jfile = open(path, 'r')
-    jmap = json.load(jfile, object_pairs_hook=OrderedDict)
-    jfile.close()
-    index = len(jmap)+1
-    jmap[index] = {}
-    jmap[index]['todo'] = args[1]
-    jmap[index]['by'] = None
-    jmap[index]['on'] = None
-    jmap[index]['will'] = None
-    jmap[index]['unwill'] = None
+    todo_file = open(path, 'r')
+    todo_map = json.load(todo_file, object_pairs_hook=OrderedDict)
+    todo_file.close()
+    index = len(todo_map)+1
+    todo_map[index] = {}
+    todo_map[index]['todo'] = args[1]
+    todo_map[index]['by'] = 'None'
+    todo_map[index]['on'] = 'None'
+    todo_map[index]['will'] = 'None'
+    todo_map[index]['unwill'] = 'None'
 
     for i in range(2, len(args)):
         if args[i] == 'by':
-            jmap[index]['by'] = args[i+1]
+            todo_map[index]['by'] = args[i+1]
         elif args[i] == 'on':
-            jmap[index]['on'] = args[i+1]
+            todo_map[index]['on'] = args[i+1]
         elif args[i] == 'will':
-            jmap[index]['will'] = args[i+1]
+            todo_map[index]['will'] = args[i+1]
         elif args[i] == 'unwill':
-            jmap[index]['unwill'] = args[i+1]
+            todo_map[index]['unwill'] = args[i+1]
 
-    jout = open(path, 'w')
-    json.dump(jmap, jout, ensure_ascii=False)
-    jout.close()
-    complete = 'done!!'
+    todo_out = open(path, 'w')
+    json.dump(todo_map, todo_out, ensure_ascii=False)
+    todo_out.close()
+    complete = 'complete!!'
     return complete
 
 def see(args):
-    jfile = open(path, 'r')
-    jmap = json.load(jfile)
-    jfile.close()
+    todo_file = open(path, 'r')
+    todo_map = json.load(todo_file)
+    todo_file.close()
     row = ['BY', 'ON', 'WILL', 'UNWILL']
     print('\n')
     print('\n')
-    print('num', end=' : \t')
-    print('TODO'.ljust(40), end='\t')
+    print('        num', end=' : \t')
+    print('TODO'.ljust(30), end='\t')
     for r in row:
-        print(r, end='      \t')
+        print(r.ljust(18), end='\t')
     print('')
-    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-    print('')
-    for i in jmap:
-        print('  ', end='')
+    print('        ', end='')
+    for i in range(120):
+        print('~', end='')
+    print('\n')
+    for i in todo_map:
+        print('          ', end='')
         print(i, end=' : \t')
-        print(jmap[i]['todo'].ljust(40), end='\t')
-        print(jmap[i]['by'], end='      \t')
-        print(jmap[i]['on'], end='      \t')
-        print(jmap[i]['will'], end='        \t')
-        print(jmap[i]['unwill'], end='      \t\n')
+        print(todo_map[i]['todo'].ljust(30), end='\t')
+        print(todo_map[i]['by'].ljust(18), end='\t')
+        print(todo_map[i]['on'].ljust(18), end='\t')
+        print(todo_map[i]['will'].ljust(18), end='\t')
+        print(todo_map[i]['unwill'].ljust(18), end='\t\n')
         print('\n')
-    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('        ', end='')
+    for i in range(120):
+        print('~', end='')
+    print('')
     print('\n')
     print('\n')
 
@@ -82,12 +87,41 @@ def erase(args):
     if len(args) <= 1:
         alart = 'eraseするリストの番号を入力してください'
         return alart
-    jfile = open(path, 'r')
-    jmap = json.load(jfile, object_pairs_hook=OrderedDict)
-    jfile.close()
-    index = str(args[1])
-    archive = jmap.pop(index)
+    #TODOファイルを開く
+    todo_file = open(path, 'r')
+    todo_map = json.load(todo_file, object_pairs_hook=OrderedDict)
+    todo_file.close()
 
+    #Archiveファイルを開く
+    archive_file = open(archive_path, 'r')
+    archive_map = json.load(archive_file, object_pairs_hook=OrderedDict)
+    archive_file.close()
+    
+    #TODOファイルからキーで削除
+    archive = todo_map.pop(args[1])
+    
+    #archive_mapに削除した値を貼り付ける
+    index = len(archive_map)+1
+    archive_map[index] = archive
+    
+    #Archiveファイルに完成した辞書を出力する
+    archive_out = open(archive_path, 'w')
+    json.dump(archive_map, archive_out, ensure_ascii=False)
+    archive_out.close()
+    
+    #TODO_mapの添字を整理する
+    new_todo_map = {}
+    count = 1
+    for v in todo_map.values():
+        new_todo_map[count] = v
+        count += 1
+    
+    #TODOファイルに書き込む
+    todo_out = open(path, 'w')
+    json.dump(new_todo_map, todo_out, ensure_ascii=False)
+    todo_out.close()
+    complete = 'complete!!'
+    return complete
 
 while True:
     print(' -> ', end=' ')
@@ -101,11 +135,13 @@ while True:
     elif command[0] == "see":
         see(command)
     elif command[0] == 'erase':
-        erase(command)
+        message = erase(command)
+        print('')
+        print(message+'\n')
     elif command[0] == "exit":
         print('Bye!!')
         sys.exit()
     else:
         print('')
-        print(' 使用できるコマンドは "add", "see", "erase", "help" です. 詳細は"help"を叩いてください')
+        print(' 使用できるコマンドは "add", "see", "erase", "exit" です.')
         print('\n')
